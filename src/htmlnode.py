cf8bb7d7b3e28367f96002eea671d3a3da128004
evanwiseman <evanwiseman2000@gmail.com>
2025-08-23 09:48:15 -0700

    added htmlnode

---
+++ src/htmlnode.py
@@ -0,0 +1,26 @@
+from typing import Optional, List, Dict
+
+class HTMLNode:
+    def __init__(
+        self, 
+        tag:Optional[str] = None, 
+        value:Optional[str] = None, 
+        children:Optional[List['HTMLNode']] = None, 
+        props:Optional[Dict[str, str]] = None
+    ):
+        self.tag = tag
+        self.value = value
+        self.children = children
+        self.props = props
+    
+    def to_html(self):
+        raise NotImplementedError()
+
+    def props_to_html(self):
+        if not self.props:
+            return ""
+        return " ".join(list(f"{key}=\"{value}\"" for key, value in self.props.items()))
+    
+    def __repr__(self):
+        return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
+    
---
+++ src/test_htmlnode.py
@@ -0,0 +1,42 @@
+import unittest
+
+from htmlnode import HTMLNode
+
+
+class TestTextNode(unittest.TestCase):
+    def test_props(self):
+        node = HTMLNode(props={
+            "href": "https://www.google.com",
+            "target": "_blank",
+        })
+        
+        self.assertEqual(
+            node.props_to_html(),
+            "href=\"https://www.google.com\" target=\"_blank\""
+        )
+    
+    def test_props_empty(self):
+        node = HTMLNode()
+        self.assertEqual(node.props_to_html(), "")
+    
+    def test_repr_empty(self):
+        node = HTMLNode()
+        self.assertEqual(str(node), "HTMLNode(None, None, None, None)")
+    
+    def test_repr(self):
+        node = HTMLNode(
+            tag="h",
+            value="This is a heading",
+            children=[HTMLNode()],
+            props={
+                "href": "https://www.google.com",
+                "target": "_blank",
+            }
+        )
+        self.assertEqual(
+            str(node),
+            "HTMLNode(h, This is a heading, [HTMLNode(None, None, None, None)], {'href': 'https://www.google.com', 'target': '_blank'})"
+        )
+
+if __name__ == "__main__":
+    unittest.main()
