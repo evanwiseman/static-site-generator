92d2ee49a8180295ade7d510d58eecec209c35fe
evanwiseman <evanwiseman2000@gmail.com>
2025-08-25 09:51:07 -0700

    updated name extract -> extract_inline

--- src/test_extract_inline.py
+++ src/test_extract_inline.py
@@ -1,7 +1,7 @@
 import unittest
 
 from textnode import TextNode, TextType
-from extract import split_nodes_delimiter, extract_markdown_images, extract_markdown_links, split_nodes_image, split_nodes_link
+from extract_inline import split_nodes_delimiter, extract_markdown_images, extract_markdown_links, split_nodes_image, split_nodes_link
 
 
 class TestConvert(unittest.TestCase):
